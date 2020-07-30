c9fb0a3773fd8f2fa3d798168133895ed3bf8be4
Kolton <Kolton.Gagnon@Gmail.com>
2020-07-30 11:05:02 -0300

    Include Tachyons float reference

--- src/rules/floats.ts
+++ src/rules/floats.ts
@@ -1,6 +1,7 @@
 import { RuleFn } from "./RuleConfig";
 import { makeRules } from "../utils";
 
+// https://github.com/tachyons-css/tachyons/blob/master/src/_floats.css
 export const floatRules: RuleFn = () =>
   makeRules("float", {
     fl: "left",
