c1b4e14fe1748978745f7c656d2b3884f3a407aa
Kimiko Kano <kkano@homebound.com>
2020-08-11 10:31:18 -0600

    Address PR comments

--- src/rules/heights.ts
+++ src/rules/heights.ts
@@ -1,45 +1,45 @@
-import { RuleFn } from './RuleConfig';
-import { makeIncRules, makeRules } from '../utils';
+import { RuleFn } from "./RuleConfig";
+import { makeIncRules, makeRules } from "../utils";
 
 export const heightRules: RuleFn = (config) => [
   // https://github.com/tachyons-css/tachyons/blob/master/src/_heights.css
 
   // Technically h1 in tachyons is 1em and ours is 1 inc
-  ...makeIncRules(config, 'h', 'height'),
+  ...makeIncRules(config, "h", "height"),
 
-  ...makeRules('height', {
-    h25: '25%',
-    h50: '50%',
-    h75: '75%',
-    h100: '100%',
-    vh25: '25vh',
-    vh50: '50vh',
-    vh75: '75vh',
-    vh100: '100vh',
+  ...makeRules("height", {
+    h25: "25%",
+    h50: "50%",
+    h75: "75%",
+    h100: "100%",
+    vh25: "25vh",
+    vh50: "50vh",
+    vh75: "75vh",
+    vh100: "100vh",
   }),
 
   ...makeRules(
-    'minHeight',
+    "minHeight",
     {
       mh0: 0,
-      mh25: '25%',
-      mh50: '50%',
-      mh575: '75%',
-      mh100: '100%',
-      mvh100: '100vh',
+      mh25: "25%",
+      mh50: "50%",
+      mh75: "75%",
+      mh100: "100%",
+      mvh100: "100vh",
     },
-    'mh'
+    "mh"
   ),
 
   ...makeRules(
-    'maxHeight',
+    "maxHeight",
     {
-      maxh0: '0',
-      maxh25: '25%',
-      maxh50: '50%',
-      maxh75: '75%',
-      maxh100: '100%',
+      maxh0: "0",
+      maxh25: "25%",
+      maxh50: "50%",
+      maxh75: "75%",
+      maxh100: "100%",
     },
-    'maxh'
+    "maxh"
   ),
 ];
--- src/rules/widths.ts
+++ src/rules/widths.ts
@@ -1,37 +1,37 @@
-import { makeIncRules, makeRules } from '../utils';
-import { RuleFn } from './RuleConfig';
+import { makeIncRules, makeRules } from "../utils";
+import { RuleFn } from "./RuleConfig";
 
 export const widthRules: RuleFn = (config) => [
-  ...makeRules('width', {
-    w25: '25%',
-    w50: '50%',
-    w75: '75%',
-    w100: '100%',
+  ...makeRules("width", {
+    w25: "25%",
+    w50: "50%",
+    w75: "75%",
+    w100: "100%",
   }),
 
   ...makeRules(
-    'minWidth',
+    "minWidth",
     {
       mw0: 0,
-      mw25: '25%',
-      mw50: '50%',
-      mw75: '75%',
-      mw100: '100%',
+      mw25: "25%",
+      mw50: "50%",
+      mw75: "75%",
+      mw100: "100%",
     },
-    'mw'
+    "mw"
   ),
 
   ...makeRules(
-    'maxWidth',
+    "maxWidth",
     {
-      maxw0: '0',
-      maxw25: '25%',
-      maxw50: '50%',
-      maxw75: '75%',
-      maxw100: '100%',
+      maxw0: "0",
+      maxw25: "25%",
+      maxw50: "50%",
+      maxw75: "75%",
+      maxw100: "100%",
     },
-    'maxw'
+    "maxw"
   ),
 
-  ...makeIncRules(config, 'w', 'width'),
+  ...makeIncRules(config, "w", "width"),
 ];
