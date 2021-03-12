184d0281f1bd3acba04f6f1d7868d2965040e10c
Stephen Haberman <shaberman@homebound.com>
2021-03-12 17:09:24 -0600

    Simplify.

--- src/rules/border-widths.ts
+++ src/rules/border-widths.ts
@@ -2,13 +2,12 @@ import { RuleFn } from "../config";
 import { newMethodsForProp, newIncrementMethods } from "../methods";
 
 // http://tachyons.io/docs/themes/borders/
-export const borderWidthRules: RuleFn = () => [
-  ...newMethodsForProp(
+export const borderWidthRules: RuleFn = () =>
+  newMethodsForProp(
     "borderWidth",
     {
       bw1: "1px",
       bw2: "2px",
     },
     "bw"
-  ),
-];
+  );
