67b3f46fd32148bac7b1ea1eaa14b6849204ac62
Stephen Haberman <shaberman@homebound.com>
2021-05-18 10:20:22 -0500

    Use module load -> FCP.

--- integration-test/.storybook/preview.js
+++ integration-test/.storybook/preview.js
@@ -1,8 +1,14 @@
 import { getFCP } from "web-vitals";
 
-getFCP((e) => {
+// Not sure this is the best way to capture this, but the metric passed to
+// `getFCP` has some relative times that are hard to track. This value `a`
+// is assigned when all story modules are loaded.
+
+const a = new Date().getTime();
+getFCP(() => {
+  const b = new Date().getTime();
   const div = document.createElement("div");
-  div.innerText = JSON.stringify(e);
+  div.innerText = `FCP ${(b - a) / 1000}`;
   document.body.insertBefore(div, document.body.firstChild);
 });
 
