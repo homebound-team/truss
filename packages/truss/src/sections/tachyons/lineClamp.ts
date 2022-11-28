5729145aee6f31b3eb5260edc52930a562df0a37
Stephen Haberman <shaberman@homebound.com>
2022-11-28 16:03:41 -0600

    fix: Restore WebkitBoxOrient. (#91)

--- packages/truss/src/sections/tachyons/lineClamp.ts
+++ packages/truss/src/sections/tachyons/lineClamp.ts
@@ -8,6 +8,9 @@ export const lineClamp: CreateMethodsFn = () => [
       overflow: "hidden",
       display: "-webkit-box",
       WebkitLineClamp: i + 1,
+      // As of 11/28/2022, this is deprecated but still necessary for lineClamp to work:
+      // https://github.com/tailwindlabs/tailwindcss-line-clamp/blob/master/src/index.js
+      WebkitBoxOrient: "vertical",
       // tailwinds doesn't add this by default, but it seems like a good default for us.
       textOverflow: "ellipsis",
     }),
