1d96bc7c2092f3a98eb6d3cc33b9b328a2b7ab39
Stephen Haberman <shaberman@homebound.com>
2025-01-23 22:44:30 -0600

    Rename file to cssProp.

--- packages/app-tachyons-fela-prop/src/jsx-dev-runtime.js
+++ packages/app-tachyons-fela-prop/src/jsx-dev-runtime.js
@@ -1,5 +1,5 @@
 import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
-import { maybeExpandCssProp } from "./render.ts";
+import { maybeExpandCssProp } from "./cssProp.ts";
 
 export function jsxDEV(type, props, ...children) {
   return _jsxDEV(type, maybeExpandCssProp(props), ...children);
--- packages/app-tachyons-fela-prop/src/jsx-runtime.js
+++ packages/app-tachyons-fela-prop/src/jsx-runtime.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx } from "react/jsx-runtime";
-import { maybeExpandCssProp } from "./render.ts";
+import { maybeExpandCssProp } from "./cssProp.ts";
 
 export function jsx(type, props, ...children) {
   return _jsx(type, maybeExpandCssProp(props), ...children);
