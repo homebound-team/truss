e8cd603ac8b47b79427348f038f24ddd85abc64a
Stephen Haberman <shaberman@homebound.com>
2025-01-24 10:07:22 -0600

    Add fast-css-prop to release.

--- release.config.js
+++ release.config.js
@@ -9,6 +9,7 @@ module.exports = {
       { prepareCmd: "yarn workspaces foreach exec npm version --no-git-tag-version ${nextRelease.version}" },
     ],
     ["@semantic-release/exec", { publishCmd: "yarn npm publish", execCwd: "packages/truss" }],
+    ["@semantic-release/exec", { publishCmd: "yarn npm publish", execCwd: "packages/fast-css-prop" }],
     "@semantic-release/github",
     ["@semantic-release/git", { assets: ["CHANGELOG.md", "package.json", "packages/*/package.json"] }],
   ],
