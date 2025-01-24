16763ff8896b45f075241a81cd17bec9479c56d4
Stephen Haberman <shaberman@homebound.com>
2025-01-24 11:58:09 -0600

    Add jsxs only in prod.

--- packages/fast-css-prop/jsx-dev-runtime.js
+++ packages/fast-css-prop/jsx-dev-runtime.js
@@ -1,7 +1,7 @@
-import { jsxDEV as _jsxDEV, Fragment, jsxs } from "react/jsx-dev-runtime";
+import { jsxDEV as _jsxDEV, Fragment } from "react/jsx-dev-runtime";
 import { maybeExpandCssProp } from "./build/cssProp.js";
 
-export { Fragment, jsxs };
+export { Fragment };
 
 export function jsxDEV(type, props, ...children) {
   return _jsxDEV(type, maybeExpandCssProp(props), ...children);
--- packages/fast-css-prop/jsx-runtime.js
+++ packages/fast-css-prop/jsx-runtime.js
@@ -1,8 +1,12 @@
-import { jsx as _jsx, Fragment, jsxs } from "react/jsx-runtime";
+import { jsx as _jsx, Fragment, jsxs as _jsxs } from "react/jsx-runtime";
 import { maybeExpandCssProp } from "./build/cssProp.js";
 
-export { Fragment, jsxs };
+export { Fragment };
 
 export function jsx(type, props, ...children) {
   return _jsx(type, maybeExpandCssProp(props), ...children);
+}
+
+export function jsxs(type, props, ...children) {
+  return _jsxs(type, maybeExpandCssProp(props), ...children);
 }
