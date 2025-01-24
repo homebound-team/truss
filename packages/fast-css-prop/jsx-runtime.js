688d1ad74e623d08e1873a2ceaaf3bfe881e48a8
Stephen Haberman <shaberman@homebound.com>
2025-01-24 11:50:28 -0600

    fix: Export the Fragment and jsxs, add fela-dom dependency.

--- packages/fast-css-prop/jsx-runtime.js
+++ packages/fast-css-prop/jsx-runtime.js
@@ -1,5 +1,7 @@
-import { jsx as _jsx } from "react/jsx-runtime";
+import { jsx as _jsx, Fragment, jsxs } from "react/jsx-runtime";
 import { maybeExpandCssProp } from "./build/cssProp.js";
+
+export { Fragment, jsxs };
 
 export function jsx(type, props, ...children) {
   return _jsx(type, maybeExpandCssProp(props), ...children);
