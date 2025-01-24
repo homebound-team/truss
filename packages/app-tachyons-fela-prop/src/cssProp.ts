76a9c721186f1a2a688f458b822e925e9c009476
Stephen Haberman <shaberman@homebound.com>
2025-01-23 22:52:40 -0600

    Update comments.

--- packages/app-tachyons-fela-prop/src/cssProp.ts
+++ packages/app-tachyons-fela-prop/src/cssProp.ts
@@ -16,33 +16,27 @@ renderer.generateClassName = (property: string, value: string, pseudo: string, m
 /**
  * Wraps React's JSX runtime (i.e. `createElement`) with `css` prop support.
  *
- * Unlike the Emotion or native Fela `css` props, we don't create a wrapping
+ * Unlike the Emotion or Fela's native `css` props, we don't create a wrapping
  * `EmotionCssPropInternal` or `FelaComponent` component, which adds another
  * React component to the component tree.
  *
- * Instead, we just merge the `css` prop into the `className` prop, and hand
- * it right back to React as a `createElement(div, ...)` etc.
+ * Instead, we directly rewrite the `css` prop into the `className` prop, and otherwise
+ * leave the `createElement(div, ...)` as-is.
  *
- * Emotion and Fela likely don't do this b/c they access their `renderer`
- * instance via a React context, which we cannot access directly b/c it
- * would surely cause "different number of hooks" errors due to conditional
- * JSX rendering. Which is dumb b/c accessing a context has no side effect,
- * so IMO contexts should be accessible w/o the hook API/restrictions.
- *
- * But, anyway, we avoid that by just accessing our `renderer` as a global
- * variable.
+ * Granted, to do this we have to cheat and access the Fela `renderer` from a global
+ * const, instead of a context, but "it's fine" (...speaking of its-fine, we could
+ * potentially use that to access our Fiber/Context from this function, sans hook).
  */
-export function maybeExpandCssProp(props: any = {}) {
+export function maybeExpandCssProp(props: any): any {
   if (props) {
     const { css, className, ...otherProps } = props;
     if (css) {
-      // Use fela/emotion to convert `{ color: "blue" }` --> `a`
-      // css._className = "black";
-      const cn = renderer.renderRule(() => css, {});
-      // Wrinkles with skipping the wrapper FelaComponent/EmotionCssPropInternal are:
-      // 1) Technically we should get renderer from `useContext` instead of a global variable :shrug:
-      // 2) Ideally in React 18 we'd use useInsertionEffect to batch style insertions but also maybe :shrug:
-      return { ...otherProps, className: className ? cn + " " + className : cn };
+      // We expect css to be a truss-created object like `{ color: "#121212" }`
+      const classNames = renderer.renderRule(() => css, {});
+      return {
+        className: className ? classNames + " " + className : classNames,
+        ...otherProps,
+      };
     }
   }
   return props;
