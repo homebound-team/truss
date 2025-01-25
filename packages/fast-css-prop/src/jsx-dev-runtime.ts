e9344ab80b1d63714eecb19fdc9906d77792dc50
Stephen Haberman <shaberman@homebound.com>
2025-01-25 10:01:18 -0600

    fix: Use hasOwn const. (#127)

--- packages/fast-css-prop/src/jsx-dev-runtime.ts
+++ packages/fast-css-prop/src/jsx-dev-runtime.ts
@@ -1,9 +1,14 @@
 import * as ReactJSXRuntime from "react/jsx-dev-runtime";
-import { maybeExpandCssProp } from "./cssProp";
+import { expandCssProp } from "./cssProp";
 export type { FastCssPropJsx as JSX } from "./jsx-namespace";
 
 export const Fragment = ReactJSXRuntime.Fragment;
 
 export const jsxDEV: typeof ReactJSXRuntime.jsxDEV = (type, props, key, isStatic, source, self) => {
-  return ReactJSXRuntime.jsxDEV(type, maybeExpandCssProp(props), key, isStatic, source, self);
+  if (!hasOwn.call(props, "css")) {
+    return ReactJSXRuntime.jsxDEV(type, props, key, isStatic, source, self);
+  }
+  return ReactJSXRuntime.jsxDEV(type, expandCssProp(props), key, isStatic, source, self);
 };
+
+const hasOwn = {}.hasOwnProperty;
--- packages/fast-css-prop/src/jsx-runtime.ts
+++ packages/fast-css-prop/src/jsx-runtime.ts
@@ -1,13 +1,21 @@
 import * as ReactJSXRuntime from "react/jsx-runtime";
-import { maybeExpandCssProp } from "./cssProp";
+import { expandCssProp } from "./cssProp";
 export type { FastCssPropJsx as JSX } from "./jsx-namespace";
 
 export const Fragment = ReactJSXRuntime.Fragment;
 
 export const jsx: typeof ReactJSXRuntime.jsx = (type, props, key) => {
-  return ReactJSXRuntime.jsx(type, maybeExpandCssProp(props), key);
+  if (!hasOwn.call(props, "css")) {
+    return ReactJSXRuntime.jsx(type, props, key);
+  }
+  return ReactJSXRuntime.jsx(type, expandCssProp(props), key);
 };
 
 export const jsxs: typeof ReactJSXRuntime.jsxs = (type, props, key) => {
-  return ReactJSXRuntime.jsxs(type, maybeExpandCssProp(props), key);
+  if (!hasOwn.call(props, "css")) {
+    return ReactJSXRuntime.jsxs(type, props, key);
+  }
+  return ReactJSXRuntime.jsxs(type, expandCssProp(props), key);
 };
+
+const hasOwn = {}.hasOwnProperty;
