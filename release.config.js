a14165eacf0f58dab2eff90cca888a9c6fead481
Stephen Haberman <shaberman@homebound.com>
2026-03-19 20:05:19 -0500

    chore: Set ci:false.

--- release.config.js
+++ release.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  // Disable CI environment detection so that semantic-release doesn't skip publishing
+  // when an open PR exists targeting the current branch (e.g. feat/v2 <- main), which
+  // causes CircleCI to set CIRCLE_PULL_REQUEST on every build for that branch.
+  ci: false,
   branches: ["main", { name: "feat/v2", range: "2.x", channel: "next" }],
   plugins: [
     "@semantic-release/commit-analyzer",
